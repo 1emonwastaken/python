7e8c4ffe9fb1b1db57014feb9aa478a144b3dba2
1emonwastaken <jayash251106@gmail.com>
2022-08-11 14:04:05 +0530

    Program to find greater of two numbers

---
+++ program19.py
@@ -0,0 +1,8 @@
+n1 = int(input("Enter the first number : "))
+n2 = int(input("Enter the second number : "))
+if n1==n2:
+    print("The two numbers are equal")
+elif n1>n2:
+    print(n1, "is greater than", n2)
+else: 
+    print(n2, "is greater than", n1)
