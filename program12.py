6b697c7e02fd992f881411a50067705e9546b6b0
lemon <97442179+1emonwastaken@users.noreply.github.com>
2022-08-11 11:58:18 +0530

    Create program12.py

---
+++ program12.py
@@ -0,0 +1,6 @@
+n10 = int(input("Enter number of Rs 10 coins : "))
+n5 = int(input("Enter number of Rs 5 coins : "))
+n2 = int(input("Enter number of Rs 2 coins : "))
+n1 = int(input("Enter number of Rs 1 coins : "))
+tot = (n10*10)+(n5*5)+(n2*2)+(n1)
+print("Total amount in piggy bank = ", tot)
