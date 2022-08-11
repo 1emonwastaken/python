1404fa9ddd491eb7e028f1d07e53191a05050d7b
lemon <97442179+1emonwastaken@users.noreply.github.com>
2022-08-11 10:57:56 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number : "))
+h = str(hex(n))
+o = str(oct(n))
+sq = n ** 0.5
+print("Hexadecimal value of ", n , "is : ", h)
+print("Octal value of ", n , "is : ", o)
+print("Square root of ", n , "is : ", sq)
