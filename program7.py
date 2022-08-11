551d0117c092c6dba511e5b69f5cee65c7d62d01
lemon <97442179+1emonwastaken@users.noreply.github.com>
2022-08-11 11:36:37 +0530

    Create program7.py

---
+++ program7.py
@@ -0,0 +1,5 @@
+r = float(input("Enter radius of cylinder : "))
+h = float(input("Enter height of cylinder : "))
+vol = 3.14*r**2*h
+print("Volume of given cylinder : ", end = ' ')
+print(round(vol, 2))
