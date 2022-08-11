4d62371ba213a17fde021b1585cfb6ed6fcc725f
lemon <97442179+1emonwastaken@users.noreply.github.com>
2022-08-11 11:35:51 +0530

    Create program5.py

---
+++ program5.py
@@ -0,0 +1,15 @@
+n1 = int(input("Enter a number : "))
+n2 = int(input("Enter another number : "))
+add = n1+n2
+sub = n1-n2
+mul = n1*n2
+div = n1/n2
+mod = n1%n2
+intdiv = n1//n2
+print("++++++++++++++--------------********************////////////////////////%%%%%%%%%%%%%%%%%%%%%%")
+print("Sum of the numbers is : " , add)
+print("Difference of the numbers is : ", sub)
+print("Product of the numbers is : ", mul)
+print("Quotient of the numbers when first number is divided by second number is : ", div)
+print("Remainder when first number is divided by second number is : ", mod)
+print("Integer part of quotient when first number is divided by second number is : ", intdiv)
