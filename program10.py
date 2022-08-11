51635073f115aa001c7097470aae1a16c4cf66a3
lemon <97442179+1emonwastaken@users.noreply.github.com>
2022-08-11 11:49:56 +0530

    Create program10.py

---
+++ program10.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+avg = (a+b)/2
+d1 = a - avg
+d2 = b - avg
+print("Average : ", avg)
+print("Deviation from first number :", d1)
+print("Deviation from second number : ", d2)
