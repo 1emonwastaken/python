b77c15b57c508088660c46d44efd5e524383dbb1
1emonwastaken <jayash251106@gmail.com>
2022-08-14 17:50:10 +0530

    init

---
+++ datatypecheck.py
@@ -0,0 +1,9 @@
+a = input("Enter any character : ")
+if a >= "A" and a <= "Z":
+    print("Entered character is an uppercase alphabet.")
+elif a >= "a" and a <= "z":
+    print("Entered character is a lowercase alphabet.")
+elif a >= "0" and a <= "9":
+    print("Entered character is a integer")
+else:
+    print("Unrecognized character.")
