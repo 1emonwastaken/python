12b2e024b87242578c581b8a3c5f70d1bce4840d
1emonwastaken <jayash251106@gmail.com>
2022-08-14 18:23:52 +0530

    init

---
+++ validtriangle.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first angle :"))
+b = float(input("Enter second angle : "))
+c = float(input("Enter third angle : "))
+if a + b + c == 180:
+    print("Given angles can form a triangle.")
+else:
+    print("Given angles can't form a triangle.") 
