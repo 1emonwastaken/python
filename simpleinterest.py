66bc4eb253720e14b11b566393c76edf4c3ad718
1emonwastaken <jayash251106@gmail.com>
2022-08-15 14:51:21 +0530

    Changed int to float

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,5 +1,5 @@
 principal = int(input("Enter principal amount : "))
-rate = int(input("Enter rate of interest (per annum): "))
-time = int(input("Enter time period (in years) : "))
+rate = float(input("Enter rate of interest (per annum): "))
+time = float(input("Enter time period (in years) : "))
 si = (principal*rate*time)/100
 print("Simple interest  : ", si)
