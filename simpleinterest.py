8a2d7a5f2e1426e5474d8463dc8ff0d48d0d6f0f
1emonwastaken <jayash251106@gmail.com>
2022-08-18 21:19:10 +0530

    Added comment "hello"

--- simpleinterest.py
+++ simpleinterest.py
@@ -3,3 +3,4 @@ rate = float(input("Enter rate of interest (per annum): "))
 time = float(input("Enter time period (in years) : "))
 si = (principal*rate*time)/100
 print("Simple interest  : ", si)
+#hello 
