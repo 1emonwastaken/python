0a8e8cb25c37cca57c5a8e93e8d32f2afd43a220
1emonwastaken <jayash251106@gmail.com>
2022-08-20 19:08:07 +0530

    init

---
+++ endwith48.py
@@ -0,0 +1,7 @@
+n = input("Enter any integer :")
+if n[-1] == '4':
+    print("The number ends with 4")
+elif n[-1] == '8':
+    print("The number ends with 8")
+else:
+    print("The number ends with neither 4 nor 8.")
