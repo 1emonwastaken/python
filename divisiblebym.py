bf974a4098589ad0be40542509e9a915767aa56b
1emonwastaken <jayash251106@gmail.com>
2022-08-20 20:05:18 +0530

    init

---
+++ divisiblebym.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the upper limit : "))
+m = int(input("Enter number to check divisibility with : "))
+print("The numbers divisible by", m, "in the range 1 -", n, "are : ")
+for i in range(1,n):
+    if i%m==0:
+        print(i)
