b427bd0dea8c94951372d97764e34978a6000008
1emonwastaken <jayash251106@gmail.com>
2022-08-20 21:06:45 +0530

    changed from str to int

--- endwith48.py
+++ endwith48.py
@@ -1,7 +1,7 @@
-n = input("Enter any integer :")
-if n[-1] == '4':
+n = int(input("Enter any integer :"))
+if n%10 == 4:
     print("The number ends with 4")
-elif n[-1] == '8':
+elif n%10 == 8:
     print("The number ends with 8")
 else:
     print("The number ends with neither 4 nor 8.")
