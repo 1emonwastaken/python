09d3a97feb4928d43a1577b23ac4bb8e6c64010c
Jayash Suryawanshi <jayash251106@gmail.com>
2022-12-28 14:41:04 +0530

    Convert decimal to hex, oct and find square root

--- numconvert.py
+++ numconvert.py
@@ -1,19 +1,8 @@
-while True:
-    print("Number Converter: ")
-    print("[1] : Decimal to Binary")
-    print("[2] : Binary to Decimal")
-    print("[3] : Octal to Decimal")
-    print("[4] : Exit")
-    inp = int(input("Enter the option you require : "))
-    if inp == 1:
-        dec = int(input("Enter the decimal number to be converted : "))
-        print("Binary equivalent of ", dec, "is : ", bin(dec))
-    elif inp == 2:
-        bnary = int(input("Enter the binary number to be converted : "), 2)
-        print("Decimal equivalent of the number is : ", int(bnary))
-    elif inp == 3:
-        octl = int(input("Enter the octal number to be converted : "), 8)
-        print("Decimal equivalent of the number is : ", int(octl))
-    elif inp == 4:
-        break
+n = int(input("Enter a number : "))
+h = str(hex(n))
+o = str(oct(n))
+sq = n ** 0.5
+print("Hexadecimal value of ", n , "is : ", h)
+print("Octal value of ", n , "is : ", o)
+print("Square root of ", n , "is : ", sq)
 
