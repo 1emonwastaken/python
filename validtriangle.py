1b0b91b01a5bba499f2e6e137bb508345ea9da3d
Jayash Suryawanshi <97442179+1emonwastaken@users.noreply.github.com>
2022-12-28 17:14:26 +0530

    Check if given angles form triangle

--- validtriangle.py
+++ validtriangle.py
@@ -4,4 +4,4 @@ c = float(input("Enter third angle : "))
 if a + b + c == 180:
     print("Given angles can form a triangle.")
 else:
-    print("Given angles can't form a triangle.") 
+    print("Given angles can't form a triangle.")  
