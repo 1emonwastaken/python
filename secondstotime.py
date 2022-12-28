96b47639c81214f4d448402950c06d23ab6ee269
Jayash Suryawanshi <97442179+1emonwastaken@users.noreply.github.com>
2022-12-28 17:12:08 +0530

    Convert seconds to hours, mins and sec

--- secondstotime.py
+++ secondstotime.py
@@ -3,4 +3,4 @@ i = inp//60
 hours = i//60
 min = i - (hours*60)
 sec = inp - ((hours*3600)+(min*60))
-print(hours , "Hours, ", min, "Minutes and ", sec,"Seconds.")
+print(hours , "Hours, ", min, "Minutes and ", sec,"Seconds.") 
